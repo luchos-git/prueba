699af4c661a41d923c5dffde416c7a38f2dc27a2
luchos-git <trevissan.luciano@gmail.com>
2026-03-27 21:11:59 -0300

    intento de comentario

---
+++ diseños moviules/pepe/documento.js
@@ -0,0 +1,30 @@
+alert("mundo hola");
+
+let a = 5;
+let b = 3;
+alert(`${a+b}`);
+
+let num1 = 1;
+let num2 = 2;
+alert(`${num1+1}`);
+alert(`${num2+5}`);
+
+let e=3;
+let f=5;
+alert(`${e}  *  ${f}  = ${e*f}`);
+
+let ingreso= prompt("ingrese un num");
+alert(`${ingreso}`);
+
+let ingreso2 = ingreso*10;
+alert(`${ingreso2}`);
+
+let a2 = prompt("Ingresa A");
+let b2 = prompt("Ingresa B");
+let a2mejor = parseInt(a2);
+let b2mejor = parseInt(b2);
+alert(`${a2mejor+b2mejor}`); /*Preguntar al profe*/
+
+/*Let = Variables (como contadores en bucles)
+Var = let pero mas antiguo e ineficiente
+Const = Constante, valores fijos, objetos y arrays que no cambian de referecia*/
